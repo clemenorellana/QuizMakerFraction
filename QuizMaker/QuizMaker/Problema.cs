1345f2b2642206d562b71c37b6750cd381c186e7
Juan Romero <helsing_000@hotmail.com>
2013-05-14 18:36:02 -0600

    Agregue la funcion de generarProblemaRandom

--- QuizMaker/QuizMaker/Problema.cs
+++ QuizMaker/QuizMaker/Problema.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+enum dificultad {easy,hard};
+
 namespace QuizMaker
 {
     public class Problema
@@ -14,6 +16,94 @@ namespace QuizMaker
 
         public Problema()
         {
+        }
+        
+        public Problema generarProblemaRandom(dificultad lvl)
+        {
+            Random n1 = new Random();
+            int nx = n1.Next(1, 4);
+            if (nx == 1)
+            {
+                operador = '+';
+            }
+            else if (nx == 2)
+            {
+                operador = '-';
+            }
+            else if (nx == 3)
+            {
+                operador = '/';
+            }
+            else
+            {
+                operador = '*';
+            }
+
+            if (lvl == dificultad.easy)
+            {
+                Random n = new Random();
+                int num = n.Next(1, 9);
+                int den = n.Next(1, 9);
+                operando1.setNumerador(num);
+                operando1.setDenominador(den);
+                n = new Random();
+                num = n.Next(1, 9);
+                den = n.Next(1, 9);
+                operando2.setNumerador(num);
+                operando2.setDenominador(den);
+                getResultado();
+            }
+            else
+            {
+                Random n = new Random();
+                int num = n.Next(10, 99);
+                int den = n.Next(10, 99);
+                operando1.setNumerador(num);
+                operando1.setDenominador(den);
+                n = new Random();
+                num = n.Next(10, 99);
+                den = n.Next(10, 99);
+                operando2.setNumerador(num);
+                operando2.setDenominador(den);
+                getResultado();
+            }
+
+            return this;
+        }
+
+        public Problema generarProblemaRandomMalos(dificultad lvl)
+        {
+
+            if (lvl == dificultad.easy)
+            {
+                Random n = new Random();
+                int num = n.Next(1, 9);
+                int den = n.Next(1, 9);
+                operando1.setNumerador(num);
+                operando1.setDenominador(den);
+                n = new Random();
+                num = n.Next(1, 9);
+                den = n.Next(1, 9);
+                operando2.setNumerador(num);
+                operando2.setDenominador(den);
+                getResultado();
+            }
+            else
+            {
+                Random n = new Random();
+                int num = n.Next(10, 99);
+                int den = n.Next(10, 99);
+                operando1.setNumerador(num);
+                operando1.setDenominador(den);
+                n = new Random();
+                num = n.Next(10, 99);
+                den = n.Next(10, 99);
+                operando2.setNumerador(num);
+                operando2.setDenominador(den);
+                getResultado();
+            }
+
+            return this;
         }
 
         public Fraccion getOperando1()
---
+++ QuizMaker/TestProject/TestProblem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestProblem
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
