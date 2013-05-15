64756b8cb20ab102c803ebd851a1c91ece8fa46a
Roger Paz <rogerpaz_1116@hotmail.com>
2013-05-14 19:45:45 -0600

    Cree la clase Test.cs y agregué la generé las posibles respuestas en la clase Problema.cs

--- QuizMaker/QuizMaker/Problema.cs
+++ QuizMaker/QuizMaker/Problema.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-enum dificultad {easy,hard};
+
 
 namespace QuizMaker
 {
@@ -13,13 +13,23 @@ namespace QuizMaker
         private Fraccion operando2;
         private char operador;
         private Fraccion resultado;
+        private Fraccion[] malos;
+
+        enum dificultad { easy, hard };
 
         public Problema()
         {
+            malos = new Fraccion[4];
         }
         
-        public Problema generarProblemaRandom(dificultad lvl)
+        public void generarProblemaRandom(int level)
         {
+            dificultad lvl = new dificultad();
+            if(level==0)
+                lvl = dificultad.easy;
+            else
+                lvl = dificultad.hard;
+
             Random n1 = new Random();
             int nx = n1.Next(1, 4);
             if (nx == 1)
@@ -38,7 +48,7 @@ namespace QuizMaker
             {
                 operador = '*';
             }
-
+            
             if (lvl == dificultad.easy)
             {
                 Random n = new Random();
@@ -68,42 +78,40 @@ namespace QuizMaker
                 getResultado();
             }
 
-            return this;
+
+
         }
 
-        public Problema generarProblemaRandomMalos(dificultad lvl)
+        public Fraccion[] generarProblemaRandomMalos()
         {
+            int cont = 0;
+            int pos = 0;
+                  
 
-            if (lvl == dificultad.easy)
+            while(cont<3)
             {
-                Random n = new Random();
-                int num = n.Next(1, 9);
-                int den = n.Next(1, 9);
-                operando1.setNumerador(num);
-                operando1.setDenominador(den);
-                n = new Random();
-                num = n.Next(1, 9);
-                den = n.Next(1, 9);
-                operando2.setNumerador(num);
-                operando2.setDenominador(den);
-                getResultado();
-            }
-            else
-            {
-                Random n = new Random();
-                int num = n.Next(10, 99);
-                int den = n.Next(10, 99);
-                operando1.setNumerador(num);
-                operando1.setDenominador(den);
-                n = new Random();
-                num = n.Next(10, 99);
-                den = n.Next(10, 99);
-                operando2.setNumerador(num);
-                operando2.setDenominador(den);
-                getResultado();
+                Random ran = new Random();
+                pos = ran.Next(0,3);
+                if(malos[pos]==null)
+                {
+                    malos[pos].setNumerador(getResultado().getNumerador()+cont+1);
+                    malos[pos].setDenominador(getResultado().getDenominador());
+
+                    cont++;
+                }
             }
 
-            return this;
+            for(int i=0;i<4;i++)
+            {
+                if(malos[i]==null)
+                {
+                    malos[pos]=getResultado();
+                }
+
+            }
+
+            
+            return malos;
         }
 
         public Fraccion getOperando1()
--- QuizMaker/QuizMaker/Test.cs
+++ QuizMaker/QuizMaker/Test.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuizMaker;
 
 namespace QuizMaker
 {
     class Test
     {
+        public Fraccion []results;
+        public Problema problem;
+
+        public void generarTest()
+        {
+            problem = new Problema();
+            problem.generarProblemaRandom(0);
+            results = problem.generarProblemaRandomMalos();
+
+        }
+
+        public bool verificarRespuesta(int posRespuesta)
+        {
+            Fraccion respuesta = results[posRespuesta];
+            if (respuesta.getNumerador() == problem.getResultado().getNumerador() && respuesta.getDenominador() == problem.getResultado().getDenominador())
+                return true;
+            return false;
+        }
     }
+
+    
+
 }
